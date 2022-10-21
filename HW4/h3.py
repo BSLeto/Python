9af345c385a024588638d59793aca2e1cb9a24a4
Leto <mikhaylichenko2000@bk.ru>
2022-10-21 17:27:41 +0300

    4 задача

---
+++ HW4/h3.py
@@ -0,0 +1,11 @@
+# 3. Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
+
+import random
+
+num = int(input('Введите длинну списка: '))
+num_list = []
+for i in range(num):
+    rand_num = random.randrange(11)
+    num_list.append(rand_num)
+print(num_list)
+print(set(num_list))
