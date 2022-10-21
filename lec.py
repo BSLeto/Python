35352b0ae7838d74b400b7e17eb286fc93c03f3f
Leto <mikhaylichenko2000@bk.ru>
2022-10-21 12:04:25 +0300

    lec_4

--- lec.py
+++ lec.py
@@ -132,4 +132,81 @@
 #         return 23
 #     else:
 #         return
-# print(f(1)) 
+# print(f(1))
+
+
+# def f(x):
+#     return x**2
+
+# g = f
+
+# print(type(g))
+
+# def calc(x):
+#     return x+10
+
+# print(calc(10))
+
+# def sum(x, y):
+#     return x+y
+
+
+# def mult(x, y):
+#     return x*y
+
+# def calc(op, a, b):
+#     print(op(a, b))
+#     # return  op(a, b)
+
+# calc(lambda x, y: x+y, 4, 5)
+
+# list = []
+
+# for i in range(1, 101):
+#     if (i%2 == 0):
+#         list.append(i)
+
+# print(list)
+
+
+# def f(x):
+#     return x**3
+
+# list = [(i, f(i)) for i in range(1, 101) if i % 2 == 0]
+# print(list)
+
+# def f(x):
+#     return x**2
+
+# list = [(i, f(i)) for i in range(1, 101) if i % 2 == 0]
+# print(list)
+
+# def select(f, col):
+#     return [f(x) for x in col]
+
+
+# def where(f, col):
+#     return [x for x in col if f(x)]
+
+
+# data = '1 2 3 4 5 8 15 23 38'.split()
+
+# res = select(int, data)
+# res = where(lambda x: not x % 2, res)
+
+# res = select(lambda x:(x, x**2), res)
+
+# print (res)
+
+
+x = 0
+y = 0
+
+def init (a, b):
+    global x
+    global y
+    x = a
+    y = b
+    
+def sum():
+    return x + y
