2e7c4632ad8c40937df86db54786ed32855eef41
Leto <mikhaylichenko2000@bk.ru>
2022-10-21 17:10:22 +0300

    Выполнил 1 задание

--- HW3/hw4.py
+++ HW3/hw4.py
@@ -3,5 +3,14 @@
 # - 45 -> 101101
 # - 3 -> 11
 # - 2 -> 10
-num = int(input("Введите число: "))
-print(bin(num)[2:])
+def int_input(message):
+    str1 = input(message)
+    if str1.isdigit():
+        result = int(str1)
+    else:
+        print ('Введено не число')
+        result = -1
+    return result
+
+num = int_input("Введите число: ")
+print(f'{num} => {bin(num)[2:]}')
