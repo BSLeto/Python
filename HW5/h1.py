eebc2e116c33d3d2aa4802af3b446ddcc96de071
Leto <mikhaylichenko2000@bk.ru>
2022-10-21 18:01:48 +0300

    1 zadanie

---
+++ HW5/h1.py
@@ -0,0 +1,14 @@
+# 1. Напишите программу, удаляющую из 
+# текста все слова содержащие "абв".
+
+text = 'Lorem Ipабвsum - это табвекст-"рабвыба", частабво используемый в печати и вабвэбабв-дизайне.\
+    Lorem Ipsum является стандартнабвой "рыбоабвй" для текстоабвв на латинице с начала XVI века.'
+
+def del_word(text):
+    text = list(filter(lambda x: 'абв' not in x, text.split()))
+    return " ".join(text)
+
+text = del_word(text)
+print (text)
+    
+                     
